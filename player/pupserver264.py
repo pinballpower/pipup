95063a72f14832c5e2368e72197576fcfbf7be4f
Pinball Power <info@pinball-power.com>
2021-10-11 15:15:23 +0200

    Use new player for background video

--- player/pupserver264.py
+++ player/pupserver264.py
@@ -7,7 +7,12 @@ import time
 import sys
 from threading import Thread
 
-from trigger import Trigger
+try:
+    from signal import SIGUSR1, SIGUSR2
+except:
+    logging.error("Can't use SIGUSR1 and SIGUSER2 on nin-UNIX-Systems")
+
+from puppack import Trigger, Playlist
 
 player=None
 bgplayer=None
@@ -16,13 +21,14 @@ bgfile=None
 
 metadata={}
 triggers = []
+playlists = {}
 
 playing = {
     "file": None,
     "priority": -1,
     }
 
-screens = ["12"]
+screen = 0
 
 class Looper(Thread):
     
@@ -39,7 +45,6 @@ class Looper(Thread):
             if player is not None:
                 if player.poll() is None:
                     # player still running
-                    logging.debug("player still running")
                     continue
                 
                 logging.info("Playback of current video has been terminated")
@@ -47,6 +52,8 @@ class Looper(Thread):
                 player = None
                 playing["file"]= None,
                 playing["priority"]= -1,
+                
+                unpause_bgplayer()
         
 
 def fileinfo(filename):
@@ -57,8 +64,22 @@ def fileinfo(filename):
     return metadata[filename]
 
 
-def play_file(filename, loop=False, priority=-1):
+def play_file(playlist, filename, loop=False, priority=-1):
     global player
+    
+    if filename is None or filename=="":
+        # Use the playlist instead
+        try:
+            pl=playlists[playlist]
+            filename=os.path.basename(pl.next_file())
+        except:
+            logging.error("couldn't find any file for playlist %s",
+                          playlist)
+    
+    if filename is None or filename=="":
+        logging.info("ignoring playfile request for empty file name")
+        return
+    
     try:
         oldplayer=None
         if player is not None:
@@ -79,16 +100,23 @@ def play_file(filename, loop=False, priority=-1):
             oldplayer.kill()
             
         # if a background loop is playing, stop it
-        stop_bgplayer()     
+        pause_bgplayer()     
 
         logging.debug("Started player")
     except Exception as e:
         logging.error("%s", e)
         
-def play_background(filename):
+def play_background(filename=None):
     global bgplayer
+    global bgfile
+    
+    if filename is None:
+        filename = bgfile
+        
+    bgfile=filename
     
     stop_bgplayer()
+    stop_player()
     absfile=fileinfo(filename)["file"]
     bgplayer=subprocess.Popen(["../hello_video.bin", "--loop", absfile]) 
          
@@ -102,15 +130,36 @@ def stop_player():
         
 def stop_bgplayer():
     global bgplayer
+    logging.debug("Killing background player")
     if bgplayer is not None:
         bgplayer.kill()
         bgplayer = None
+        
+def pause_bgplayer():
+    global bgplayer
+    if bgplayer is not None:
+        try: 
+            bgplayer.send_signal(SIGUSR1)
+            logging.debug("Sent USR1 to background player")
+        except:
+            stop_bgplayer()
+        
+
+def unpause_bgplayer():
+    global bgplayer
+    if bgplayer is not None:
+        try: 
+            bgplayer.send_signal(SIGUSR2)
+            logging.debug("Sent USR2 to background player")
+        except:
+            play_background()
+    else:
+        play_background()
    
 def process_triggers(event):
     found=False
     for t in triggers:
-        logging.debug("Checking %s",t)
-        if t.screennum in screens and t.trigger == event:
+        if t.screennum==screen and t.trigger == event:
             found = True
             logging.debug("Processing trigger %s", t)
             process_trigger(t)
@@ -129,25 +178,35 @@ def process_trigger(t):
             
     elif t.loop=="StopPlayer":
         if t.priority > playing["priority"]:
+            logging.debug("Stopping playback")
             stop_player 
+        else:
+            logging.debug("Not stopping playback as playing video has higher priority")
             
     elif t.loop=="Normal" or t.loop=="":
-        play_file(t.playfile, loop=False, priority=t.priority)
+        logging.debug("Start normal playback of %s/%s, priority %s",
+                          t.playlist, t.playfile, t.priority)
+        play_file(t.playlist, t.playfile, loop=False, priority=t.priority)
             
     elif t.loop=="Loop":
-        play_file(t.playfile, loop=True, priority=t.priority)
+        logging.debug("Start loop of %s/%s, priority %s",
+                          t.playlist, t.playfile, t.priority)
+        play_file(t.playlist, t.playfile, loop=True, priority=t.priority)
         
     elif t.loop=="SkipSamePrty":
         if playing["priority"] != t.priority:
-            logging.debug("Start playback of %s, priority %s",
-                          t.playfile, playing["file"],t.priority)
-            play_file(t.playfile, loop=False, priority=t.priority)
+            logging.debug("Start playback of %s/%s, priority %s",
+                          t.playlist, t.playfile, t.priority)
+            play_file(t.playlist, t.playfile, loop=False, priority=t.priority)
         else:
-            logging.debug("Skipping %s as file %s with same priority %s is already playing",
-                          t.playfile, playing["file"],t.priority)
+            logging.debug("Skipping %s/%s as file %s with same priority %s is already playing",
+                          t.playlist, t.playfile, playing["file"],t.priority)
+    elif t.loop=="SetBG":
+        play_background(t.playfile)
+        
     else:
         logging.info("Trigger loop type %s not yet implemented", t.loop)
-                
+       
             
             
 @route('/play/<filename>')
@@ -168,7 +227,6 @@ def read_files(basedir):
     ## Get a list of all files
     for f in glob(basedir+"/*/*.h264"):
         key=os.path.basename(f)[:-5].lower()
-        logging.error(key)
         metadata[key]={"file": os.path.abspath(f)}
         
     ## Read the triggers
@@ -178,13 +236,21 @@ def read_files(basedir):
             triggers.append(t)
             logging.debug("Added trigger %s", t)
             
+    ## Read the playlists
+    with open(basedir+"/playlists.pup", 'r', encoding='utf-8') as infile:
+        for line in infile:
+            p = Playlist(line, basedir)
+            playlists[p.folder]=p
+            logging.debug("Added playlist %s", p)
+            
 def main():
     try:
         datadir=sys.argv[1]
+        screen=sys.argv[2]
     except:
         datadir="."
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     read_files(datadir)
 
     looperthread=Looper()
@@ -192,7 +258,6 @@ def main():
 
     run(host="localhost", port=5000)
     
-
 
 if __name__ == '__main__':
     main()
